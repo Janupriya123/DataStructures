aa4d3633e8ef5f2969f416ee2b1d7a14920fca93
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-01-28 00:15:16 +0530

    Gcd

---
+++ Math/Gcd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class A
+  {
+    public static void main(String[]args)
+    {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      int temp=0;
+      while(b!=0)
+        {
+          temp=b;//store value of b in temp
+          b=a%b;//div a by b
+          a=temp;//store temp in a
+        }
+      System.out.println("Gcd is:"+a);
+      sc.close();
+    }
+  }
+          
