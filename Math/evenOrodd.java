c0066c7ba398767f3835c44e6693cc9f3f8c12c2
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-01-27 23:26:01 +0530

    evenOrodd

--- Math/evenOrodd.java
+++ Math/evenOrodd.java
@@ -3,9 +3,9 @@ class A
 {
  public static void main(String[] args)
   {
-    Scanner sc=new Scanner(System.in);
-    int n=sc.nextInt();
-    if(n%2==0)
+    Scanner sc=new Scanner(System.in); 
+    int n=sc.nextInt();//get input from User
+    if(n%2==0)//if Remainder is 0 then even else Odd
     {
        System.out.println("Even");
      }
