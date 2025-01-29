25b39377c9ea62b6ddff6c69107dd0eb2d52f1ce
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-01-29 20:50:17 +0530

    reverseNumber

--- Math/reverseNumber.java
+++ Math/reverseNumber.java
@@ -1,17 +1,17 @@
-Method 1:Using Math//Method 1
+Method 1:Using Math // Method 1
 import java .util.Scanner;
 class A
   {
     public static void main(String[]args)
     {
       Scanner sc=new Scanner(System.in);
-      int n=sc.nextInt();//get input from user
+      int n=sc.nextInt(); // get input from user
       int ans=0;
       while(n!=0)
         {
-          int l=n%10;//get last digit
+          int l=n%10; // get last digit
           ans=ans*10+l;
-          n=n/10;//remove last digit
+          n=n/10; // remove last digit
         }
       System.out.println("Reverse of a number:"+ans);
       sc.close();
@@ -26,9 +26,9 @@ class A
     {
       Scanner sc=new Scanner(System.in);
       int n=sc.nextInt();
-      String s=String.valueOf(n);//convert int to String
-      StringBuilder s1=new StringBuilder(s);//convert String to StringBuilder
-      String ans=s1.reverse().toString();//using reverse function reverse the value
+      String s=String.valueOf(n); // convert int to String
+      StringBuilder s1=new StringBuilder(s); // convert String to StringBuilder
+      String ans=s1.reverse().toString(); // using reverse function reverse the value
       System.out.println("Reverse of a number:"+ans);
       sc.close();
     }
