28621507e5df5aaa9630ec8a9ef677d69781e0b5
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-01-29 19:55:17 +0530

    Divisors

---
+++ Math/Divisors.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class A
+  {
+    public static void main(String[]args)
+    {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+       
+       for(int i=1;i*i<=a;i++)// iterate till i*i<=a
+       {
+        if(a%i==0)//one divisor of the number
+        {
+          System.out.println(i);
+          if(a/i!=i)
+          {
+              System.out.println(a/i);//another divisor of the number
+          }
+        }
+       }
+      sc.close();
+    }
+  }
