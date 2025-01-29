7608bdc5212f68128b3e1383d42b806c1ef14b15
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-01-29 20:52:08 +0530

    countDigits

--- Math/countDigits.java
+++ Math/countDigits.java
@@ -4,14 +4,14 @@ class A
    public static void main(String[]args)
     {
       Scanner sc=new Scanner(System.in);
-      int n=sc.nextInt();//get input from user
-      int count=0;//variable for counting digits
+      int n=sc.nextInt(); // get input from user
+      int count=0; // variable for counting digits
       while(n!=0)
         {
-          count++;//count the digits
-          n=n/10;//remove last digit after counting
+          count++; // count the digits
+          n=n/10; // remove last digit after counting
         }
-      System.out.println("Digits count:"+count);//display the results
+      System.out.println("Digits count:"+count); // display the results
       sc.close();
     }
   }
