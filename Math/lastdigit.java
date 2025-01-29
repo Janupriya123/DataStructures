11136cc3263465aca95bd2f8ba6e1438c7a92927
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-01-29 20:50:51 +0530

    lastdigit

--- Math/lastdigit.java
+++ Math/lastdigit.java
@@ -4,8 +4,8 @@ class A
     public static void main(String[]args)
     {
       Scanner sc=new Scanner(System.in);
-      int n=sc.nextInt();//get input from user
-      System.out.println("Last Digit:"+n%10);//%10 will give last digit of a number
+      int n=sc.nextInt(); // get input from user
+      System.out.println("Last Digit:"+n%10); // %10 will give last digit of a number
       sc.close();
     }
   }
