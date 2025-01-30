45101ba4c143f5e77b4a15652477fad5877eb7f2
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-01-30 23:16:03 +0530

    MaximumAndMinimum

--- Array/MaximumAndMinimum.java
+++ Array/MaximumAndMinimum.java
@@ -11,11 +11,11 @@ import java.util.Arrays;
         int arr[]=new int[n];
         for(int i=0;i<n;i++)
           {
-            arr[i]=sc.nextInt(); // Get array Elements from 
+            arr[i]=sc.nextInt(); // Get array Elements from Users
           }
-        Arrays.sort(arr);
-        System.out.println("Maximum Element:"+arr[n-1]);
-        System.out.println("Minimum Element:"+arr[0]);
+        Arrays.sort(arr); // Sort the array
+        System.out.println("Maximum Element:"+arr[n-1]); // Print last element which is the Maximum element
+        System.out.println("Minimum Element:"+arr[0]); // Print First element which is the minimum element
         sc.close();
       }
     }
@@ -34,21 +34,21 @@ class A
         {
           arr[i]=sc.nextInt();
         }
-      int min=Integer.MAX_VALUE;
-      int max=Integer.MIN_VALUE;
+      int min=Integer.MAX_VALUE; // Assign Integer Maximum
+      int max=Integer.MIN_VALUE; // Assign Integer Minimum
       for(int i=0;i<n;i++)
         {
-          if(arr[i]>max)
+          if(arr[i]>max) // if current element greater than max 
           {
-            max=arr[i];
+            max=arr[i]; // replace max as current element
           }
-          else if(arr[i]>min)
+          else if(arr[i]<min) // else if current element greater than min
           {
-            min=arr[i];
+            min=arr[i]; // replace min as current element
           }
         }
-       System.out.println("Maximum Element:"+max]);
+       System.out.println("Maximum Element:"+max);
         System.out.println("Minimum Element:"+min);
-      sc.clsoe();
+      sc.close();
     }
   }
