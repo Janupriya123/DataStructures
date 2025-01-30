b15a5d04268e2f416ecc53f4fe37ea28f7cbfffe
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-01-30 22:35:32 +0530

    ReversalOfArray

--- Array/ReversalOfArray.java
+++ Array/ReversalOfArray.java
@@ -15,7 +15,7 @@ class A
       int ans[]=new int[n]; // create temporary Array
       for(int i=0;i<n;i++)
         {
-          ans[n-i-1]=arr[i]; // 
+          ans[n-i-1]=arr[i]; // store from first element to last element of original array   
         }
       System.out.println("Array Reverse:");
       for(int num:ans)
@@ -27,7 +27,7 @@ class A
     }
   }
 
-Method 2 : Using Two Pointers
+Method 2 : Using Two Pointers // Method 2
   
 import java.util.Scanner;
 class A
@@ -42,11 +42,11 @@ class A
          arr[i]=sc.nextInt();
 
       }
-      int l=0;
-      int r=n-1;
+      int l=0; // Traverse from 0
+      int r=n-1; // Traverse from n-1
       while(l<r)
       {
-         int temp=arr[r];
+         int temp=arr[r]; // Swap arr[l] and arr[r]
          arr[r]=arr[l];
          arr[l]=temp;
          l++;
