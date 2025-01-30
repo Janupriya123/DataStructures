aa6be9f2341ac503b042a1e71820e4b71297874f
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-01-30 22:26:08 +0530

    ReversalOfArray

---
+++ Array/ReversalOfArray.java
@@ -0,0 +1,63 @@
+Method 1 : Using Temporary Array // Method 1
+
+import java.util.Scanner;
+class A
+  {
+    public static void main(String[]args)
+    {
+       Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int arr[]=new int[n]; // original array
+      for(int i=0;i<n;i++)
+        {
+          arr[i]=sc.nextInt();
+        }
+      int ans[]=new int[n]; // create temporary Array
+      for(int i=0;i<n;i++)
+        {
+          ans[n-i-1]=arr[i]; // 
+        }
+      System.out.println("Array Reverse:");
+      for(int num:ans)
+        {
+          System.out.print(num+" ");
+        }
+      sc.close();
+      
+    }
+  }
+
+Method 2 : Using Two Pointers
+  
+import java.util.Scanner;
+class A
+{
+   public static void main(String[]args)
+   {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int arr[]=new int[n];
+      for(int i=0;i<n;i++)
+      {
+         arr[i]=sc.nextInt();
+
+      }
+      int l=0;
+      int r=n-1;
+      while(l<r)
+      {
+         int temp=arr[r];
+         arr[r]=arr[l];
+         arr[l]=temp;
+         l++;
+         r--;
+      }
+      System.out.println("Array Reversal");
+      for(int num:arr)
+      {
+         System.out.print(num+" ");
+      }
+      sc.close();
+   }
+
+}
