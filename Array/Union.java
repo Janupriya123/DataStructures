9972fc58b12a4999fa88fbae6faaaec5693b75b1
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-02-02 23:56:45 +0530

    UnionOfArrays

--- Array/Union.java
+++ Array/Union.java
@@ -40,3 +40,75 @@ class A
       sc.close();
     }
   }
+
+Method 2: Using Two Pointers // Method 2
+
+import java.util.Scanner;
+import java.util.ArrayList;
+class A
+  {
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt(); 
+      int m=sc.nextInt();
+      int a[]=new int[n]; // first array
+      int b[]=new int[m]; // second array
+      for(int i=0;i<n;i++)
+        {
+          a[i]=sc.nextInt();
+        }
+      for(int i=0;i<m;i++)
+        {
+          b[i]=sc.nextInt();
+        }ArrayList<Integer> ans=new ArrayList<>();
+int i=0;
+int j=0;
+while(i<a.length && j<b.length)
+{
+    if(a[i]<=b[j]) // if a[i]<=b[j]
+    {
+        if(ans.size()==0 || ans.get(ans.size()-1)!=a[i]) 
+        ans.add(a[i]);
+        i++;
+         
+    }
+    else if(a[i]>b[j])
+    {
+        if(ans.size()==0 || ans.get(ans.size()-1)!=b[j])
+        ans.add(b[j]);
+        
+        j++;
+         
+    }
+   
+}
+while(i<a.length)
+{
+  if(ans.size()==0 || ans.get(ans.size()-1)!=a[i])
+  {
+    ans.add(a[i]);
+    i++;
+  }
+     
+}
+while(j<b.length)
+{
+  if(ans.size()==0 || ans.get(ans.size()-1)!=b[j])
+  {
+    ans.add(b[j]);
+    j++;
+  }
+     
+}
+int k=0;
+while(k<ans.size())
+{
+  System.out.print(ans.get(k)+" ");
+  k++;
+}
+sc.close();
+ 
+    }
+}
+
