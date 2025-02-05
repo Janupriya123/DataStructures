8e3fb32e3ad37c12cc8df624aad7d642d8e72f61
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-02-05 20:38:25 +0530

    BestTimeToBuyStocks

---
+++ Array/BestTimeToBuyStocks.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class BestTimeToBuyStocks
+  {
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      int arr[]=new int[n];
+      for(int i=0;i<n;i++)
+        {
+          arr[i]=sc.nextInt();
+        }
+        int max=0; // Initialize max profit as 0
+        int min=arr[0]; // Initialize min stock price to buy as arr[0]
+        for(int i=1;i<arr.length;i++)
+        {
+            int cur=arr[i]-min; // Subtract the current stock and minimum stock
+            max=Math.max(max,cur); // find maximum profit among max and cur
+            min=Math.min(min,arr[i]); // find minimum stock among min and current stock
+        }
+      System.out.println("Maximum Profit:"+max);
+      sc.close();
+    }
+  }
