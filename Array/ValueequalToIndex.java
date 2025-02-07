563b9d89438f71be768aa9b2f9bfd41c826f59ce
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-02-07 21:18:59 +0530

    ValueequalToIndex

--- Array/ValueequalToIndex.java
+++ Array/ValueequalToIndex.java
@@ -1,1 +1,25 @@
+Using Loops // Using loops
+
+  import java.util.Scanner; 
+class A
+  {
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt(); // Size of the Array
+      int arr[]=new int[n];
+      for(int i=0;i<n;i++)
+        {
+          arr[i]=sc.nextInt();
+        }
+      for(int i=0;i<n;i++)
+        {
+          if(arr[i]==i+1) // if current element == i+1
+          {
+            System.out.print(arr[i]);
+          }
+        }
+       sc.close();
+    }
+  }
 
