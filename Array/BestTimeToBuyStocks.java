5b9a4d628a84025bb9762f10eaecfbbddbc2edcd
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-02-09 20:59:06 +0530

    BestTimeToBuyStocks.java

--- Array/BestTimeToBuyStocks.java
+++ Array/BestTimeToBuyStocks.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class BestTimeToBuyStocks
   {
     public static void main(String[] args)
-    {
+    { 
       Scanner sc=new Scanner(System.in);
       int n=sc.nextInt(); 
       int arr[]=new int[n];
