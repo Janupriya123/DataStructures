e2cec9fddf812bbc9cebeeba826d904b437ae75f
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-02-11 23:09:26 +0530

    SearchInRotatedSortedArray.java

--- Binary Search/SearchInRotatedSortedArray.java
+++ Binary Search/SearchInRotatedSortedArray.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class A   
   { 
     public static void main(String[] args)
-    {
+    { 
       Scanner sc=new Scanner(System.in);
       int n=sc.nextInt(); // Size of the Array
       int nums[]=new int[n]; 
