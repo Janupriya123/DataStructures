d61a13188fc3c11240992dd19c0cc8c159310b4a
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-02-12 21:53:44 +0530

    Sort 0,1,2.java

--- Array/Sort 0,1,2.java
+++ Array/Sort 0,1,2.java
@@ -4,7 +4,7 @@
  import java.util.Arrays;
  class Main
  {
-  public static void main(String[] args) {
+  public static void main(String[] args) { 
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
     int arr[]=new int[n];
