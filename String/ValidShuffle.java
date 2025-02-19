4d996193a07b5eab468527d02ed23e7d0f423f79
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-02-19 23:35:30 +0530

    ValidShuffle.java

--- String/ValidShuffle.java
+++ String/ValidShuffle.java
@@ -7,7 +7,7 @@ class A
       String s1=sc.nextLine();
       String s2=sc.nextLine();
       String res=sc.nextLine();
-      if((s1.length()+s2.length())!=res.length())
+      if((s1.length()+s2.length())!=res.length()) // if s1 +s2 length is same as res length
       {
         System.out.println("Not a Valid Shuffle");
       }
@@ -15,24 +15,24 @@ class A
       int j=0;
       int k=0;
       int f=0;
-      while(K<res.length())
+      while(K<res.length()) // k <res
         {
-          if(i<s1.length() && s1.charAt(i)==res.charAt(k))
+          if(i<s1.length() && s1.charAt(i)==res.charAt(k)) //i<s1 and character at i in s1 == to character at k in res,then i++
           {
             i++;
           }
-          else if(j<s2.length() && s2.charAt(j)==res.charAt(k))
+          else if(j<s2.length() && s2.charAt(j)==res.charAt(k))  //j<s2 and character at j in s2 == to character at k in res,then j++
           {
             j++;
           }
-          else
+          else // if not equal initialize f=1 and break
           {
             f=1;
             break;
           }
           k++;
         }
-      if(f==1)
+      if(f==1) // if f==1,then not a valid shuffle
       {
         System.out.println("Not a Valid Shuffle");
       }
