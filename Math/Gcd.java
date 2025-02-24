39730e1fb44da2f909318a1fef6bd3f9f582065a
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-02-24 22:06:56 +0530

    Gcd.java

--- Math/Gcd.java
+++ Math/Gcd.java
@@ -4,7 +4,7 @@ class A
     public static void main(String[]args)
     {
       Scanner sc=new Scanner(System.in);
-      int a=sc.nextInt();
+      int a=sc.nextInt(); 
       int b=sc.nextInt();
       int temp=0;
       while(b!=0)
