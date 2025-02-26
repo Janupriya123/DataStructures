ee4fcda5b8776c91e53ea65f5b06f519061fd297
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-02-26 18:01:24 +0530

    CountTheReversal.java

--- String/CountTheReversal.java
+++ String/CountTheReversal.java
@@ -1,24 +1,36 @@
- if(s.length()%2!=0)
+import java.util.Scanner;
+class A
+  { 
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      String s=sc.nextLine();
+      if(s.length()%2!=0) // is string is odd length
         {
-            return -1;
+            System.out.println("-1");
+             return;
         }
         int size=0;
         int open=0;
         for(int i=0;i<s.length();i++)
         {
             char ch=s.charAt(i);
-            if(ch=='{')
+            if(ch=='{') // if current character is open bracket,increment size
             {
                 size++;
             }
-            else if(size>0)
+            else if(size>0) //  if current character is closed bracket and size is > than 0,decrement size
             {
                 size--;
             }
             else
             {
-                open++;
+                open++; 
             }
             
         }
-        return (int)(Math.ceil(size/2.0)+Math.ceil(open/2.0));
+      System.out.println((int)(Math.ceil(size/2.0)+Math.ceil(open/2.0)));
+     sc.close();
+    }
+  }
+     
