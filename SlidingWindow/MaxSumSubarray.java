af1bb5244fd3ffedbddfeef5b1a3a56322eefb77
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-03-28 19:23:37 +0530

    MaxSumSubarray.java

--- SlidingWindow/MaxSumSubarray.java
+++ SlidingWindow/MaxSumSubarray.java
@@ -1,21 +1,32 @@
-int c=0;
-int s=0;
-int i=0;
-int j=0;
-int max=Integer.MIN_VALUE;
-while(j<arr.length)
-{
-    s+=arr[j];
-    c++;
-    if(c==k)
-    {
-      max=Math.max(s,max);
-      s-=arr[i];
-      i++;
-      c--;
-      }
-      j++;
+import java.util.Scanner; 
+public class Main { 
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);  
+        int n=sc.nextInt(); 
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+            {
+                arr[i]=sc.nextInt();
+            }
+        int c=0;
+        int s=0;
+        int i=0;
+        int j=0;
+        int max=Integer.MIN_VALUE;
+        while(j<arr.length)
+       {
+          s+=arr[j];
+          c++;
+         if(c==k)
+      {
+         max=Math.max(s,max);
+        s-=arr[i];
+        i++;
+        c--;
+    }
+        j++;
     }
    System.out.println(max);
+   sc.close();
 }
 }
