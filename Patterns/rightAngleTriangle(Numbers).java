dd219f839533f12f3855a13f88884ff97a7c7475
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-04-14 23:13:30 +0530

    rightAngleTriangle(Numbers).java

--- Patterns/rightAngleTriangle(Numbers).java
+++ Patterns/rightAngleTriangle(Numbers).java
@@ -8,7 +8,7 @@ class A
       for(int i=1;i<=n;i++)
         {
           for(int j=1;j<=n;j++)
-            {
+            { 
               System.out.print(j);
             }
           System.out.println();
