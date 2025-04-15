4bb40e5aadac2d801fca3d3ea3d476e64956fca8
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-04-15 23:51:51 +0530

    missingNum.java

--- Array/missingNum.java
+++ Array/missingNum.java
@@ -1,9 +1,22 @@
-  int n = arr.length + 1;
-        long total =(long) n * (n + 1) / 2;
-
-         long sum = 0;
+import java.util.Scanner; 
+class A
+  {
+    public static void main(String[] args)
+    {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();  
+      int arr[]=new int[n];
+      for(int i=0;i<n;i++)
+        {
+          arr[i]=sc.nextInt();
+        }
+        int n1 = arr.length + 1;
+        long total =(long)  n1* (n1 + 1) / 2;
+        long sum = 0;
         for (int num : arr) {
             sum += num;
         }
-
-        return (int) (total - sum);
+        System.out.print((int) (total - sum));
+        sc.close();
+    }
+  }
