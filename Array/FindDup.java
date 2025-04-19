59e9a61209bde7226b34bd55f969ab2608cb811a
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-04-19 23:50:21 +0530

    Update FindDup.java

--- Array/FindDup.java
+++ Array/FindDup.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+     int s=0;
+     int f=0;
+     do
+     {
+        s=nums[s];
+        f=nums[nums[f]];
 
+     }
+     while(s!=f);
+     s=0;
+     while(s!=f)
+     {
+        s=nums[s];
+        f=nums[f];
+     }
+    System.out.println(s);
+    sc.close();
+
+    }
+}
