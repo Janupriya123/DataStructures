05ff1864c2686c5a5ce576c8b62c68bd17a60854
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-04-21 21:09:50 +0530

    ThreeSum.java

--- Array/ThreeSum.java
+++ Array/ThreeSum.java
@@ -1,4 +1,14 @@
- Arrays.sort(arr);
+import java.util.Scanner;
+class Main {
+         public static void main(String[] args) 
+        {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
         int j=0;
         int k=arr.length-1;
         for(int i=0;i<arr.length;i++)
@@ -27,5 +37,6 @@
         }  
            System.out.print("true");
            return;
+         sc.close();
     }
 }
