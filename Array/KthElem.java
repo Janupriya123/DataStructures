3afcfe54f5d08f20f6ab29474524f18758eb1757
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-04-22 22:39:58 +0530

    KthElem.java

--- Array/KthElem.java
+++ Array/KthElem.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+class  A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t=sc.nextInt();
+        int  a[] = new int[n];
+        int b[]=new int[t];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+         for (int i = 0; i < t; i++) {
+             b[i] = sc.nextInt();
+        }
+        int k=sc.nextInt();
         int arr[]=new int[a.length+b.length];
         int m=0;
         int i=0;
         int j=0;
         if(k<=0 || k>a.length+b.length)
         {
-            return -1;
+             System.out.print("-1");
+             return;
         }
         while(i<a.length && j<b.length)
         {
@@ -27,4 +43,8 @@
         {
             arr[m++]=b[j++];
         }
-        return arr[k-1];
+        System.out.print(arr[k-1]);
+        return; 
+        sc.close();   
+    }
+}
