f27af429c413b6b5063e64d0fdd66fe248b5e503
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-04-29 22:34:15 +0530

    Create SearchInInfiniteArray.java

---
+++ Binary Search/SearchInInfiniteArray.java
@@ -0,0 +1,18 @@
+        long l=0;
+        long r= Long.MAX_VALUE;
+        long mid=0;
+        long ans=0;
+        while(l<=r)
+        {
+              mid=l+(r-l)/2;
+              if(Runner.get(mid)==1)
+              {
+                  ans=mid;
+                  r=mid-1;
+              }
+              else
+              {
+                  l=mid+1;
+              }
+        }
+        return ans;
