bda41d951382340d7a2a8ca5946691c7a226f2dc
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-05-11 23:37:13 +0530

    MaxSumSubarray.java

--- SlidingWindow/MaxSumSubarray.java
+++ SlidingWindow/MaxSumSubarray.java
@@ -1,7 +1,7 @@
 import java.util.Scanner; 
 public class Main {  
     public static void main(String[] args) {  
-        Scanner sc = new Scanner(System.in);  
+        Scanner sc = new Scanner(System.in);   
         int n=sc.nextInt(); 
         int arr[]=new int[n];
         for(int i=0;i<n;i++)
