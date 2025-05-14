6705aaac7e6a7a5894dfe2d7426dc6b68ae1c980
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-05-14 23:46:28 +0530

    MaximumAndMinimum.java

--- Array/MaximumAndMinimum.java
+++ Array/MaximumAndMinimum.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
   class A
     {
       public static void main(String[] args) 
-      {
+      { 
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int arr[]=new int[n];
