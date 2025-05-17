11f094ed68e5248a1a1e82d5220078c1b7258570
Janu Priya E <149947823+Janupriya123@users.noreply.github.com>
2025-05-17 23:29:59 +0530

    AsciiPrimeCheck.java

--- Math/AsciiPrimeCheck.java
+++ Math/AsciiPrimeCheck.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class rough {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner sc=new Scanner(System.in);
         String s=sc.nextLine();
         int sum = 0;
